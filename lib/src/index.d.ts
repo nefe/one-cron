efd587f4d380b523856f2307bd8816ec3b2f70c7
Perkin <perkin.pj@alibaba-inc.com>
2018-05-30 19:54:41 +0800

    feat: add cronUtils

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
-import * as React from 'react';
-import './index.css';
+import * as React from "react";
+import "./index.css";
 export default class OneCron extends React.Component<any, any> {
     render(): JSX.Element;
 }
--- lib/src/utils.d.ts
+++ lib/src/utils.d.ts
@@ -1,2 +1,7 @@
-declare const escapeRegExp: (s: string) => string;
-export { escapeRegExp };
+/**
+ * 得到一个数组
+ * @param length 数组长度
+ * @param beginNum 第一个元素值
+ */
+declare function getArr(length: number, beginNum?: number, arr?: any[]): number[];
+export { getArr };
