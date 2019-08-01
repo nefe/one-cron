707f974acbc6e0bc563f2318cf0cd21ba85a5133
fanyongchao-js <fanyongchao.fyc@alibaba-inc.com>
2019-08-01 20:10:26 +0800

    fix: default bugfix

--- demo/index.tsx
+++ demo/index.tsx
@@ -39,8 +39,6 @@ class App extends React.Component<Prop, State> {
           onChange={exp => this.handleChange(exp)}
           cronExpression={this.state.cronExpression}
           showRecentTime={true}
-          beginTime={8}
-          endTime={21}
         />
       </div>
     );
