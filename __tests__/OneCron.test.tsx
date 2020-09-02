14ba0e6700629861bd3046cc65f31abe1507ef20
bzhangzju <zhangbing109@gmail.com>
2020-09-02 12:04:55 +0800

    test: add unit test for cronValidate function

--- __tests__/OneCron.test.tsx
+++ __tests__/OneCron.test.tsx
@@ -1,7 +1,7 @@
 import * as Enzyme from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
 import * as React from "react";
-import OneCron from "../src";
+import OneCron, { cronValidate } from "../src";
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -51,4 +51,15 @@ it("dayOfWeekOneBased prop is false", () => {
   expect(cronOne.find(".errorCronExp").length).toBe(0);
   // 应该选中周日和周六
   expect(cronOne.find('.schedule-period.week .cron-select-wrapper .ant-select-selection__choice__content').text()).toBe('SunSat');
+});
+
+describe('cronValidate', () => {
+  it('week schedule', () => {
+    expect(cronValidate('0 0 2 ? * 1,2,3,4,5,6,7')).toBe(true);
+    expect(cronValidate('0 0 2 ? * 0,8')).toBe(false);
+    expect(cronValidate('0 0 2 ? * 0,1,2,3,4,5,6', false)).toBe(true);
+    expect(cronValidate('0 0 2 ? * 7,8', false)).toBe(false);
+    // cronValidate函数支持dayOfWeek字段是SUN,MON这种格式，但是OneCron组件并不支持这种格式，只支持像1,2这种数据格式
+    expect(cronValidate('0 0 2 ? * SUN,MON,TUE,WED,THU,FRI,SAT')).toBe(true);
+  });
 });
